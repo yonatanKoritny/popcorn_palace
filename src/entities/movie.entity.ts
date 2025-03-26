16123b94cb608a0a493c4412456a2137d12d337f
yonatanKoritny <megayoni55@gmail.com>
2025-03-27 00:25:27 +0200

    Movies Module: initial implementation

---
+++ src/entities/movie.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Movie {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  genre: string;
+
+  @Column()
+  duration: number;
+
+  @Column()
+  rating: string;
+
+  @Column({ name: 'release_year' })
+  releaseYear: number;
+}
---
+++ src/movies/movies.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Movie } from 'src/entities/movie.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class MoviesService {
+  constructor(
+    @InjectRepository(Movie)
+    private moviesRepository: Repository<Movie>,
+  ) {}
+
+  async findAll(): Promise<Movie[]> {
+    return this.moviesRepository.find();
+  }
+}
